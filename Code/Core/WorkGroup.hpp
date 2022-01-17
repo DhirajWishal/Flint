29829392366dfbb2e6c748d0fac7d54af6158f51
Dhiraj Wishal <wishaldhiraj@gmail.com>
2022-01-17 19:25:38 +0530

    Added a new object.
    * Added a new object: WorkGroup.
    * This object contains a list of workers where the work is divided among them in a deterministic manner.

---
+++ Code/Core/WorkGroup.hpp
@@ -0,0 +1,69 @@
+// Copyright 2021 Dhiraj Wishal
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "Worker.hpp"
+
+namespace Flint
+{
+	/**
+	 * Work group class.
+	 * This class contains a bunch of workers, where the work is divided among them in a deterministic manner.
+	 */
+	class WorkGroup
+	{
+	public:
+		/**
+		 * Constructor.
+		 * Construct the work group using the worker count.
+		 * 
+		 * @param groupSize The number of workers in the work group.
+		 */
+		explicit WorkGroup(uint64 groupSize) : mWorkers(groupSize) {}
+
+		/**
+		 * Issue work to a worker to execute.
+		 * Work is assigned in a deterministic way (index % groupSize).
+		 *
+		 * @tparam Function The function type.
+		 * @tparam Arguments The function arguments.
+		 * @tparam ReturnType The return type of the function. This can either be specified explicitly or can be deduced implicitly.
+		 * @param function The function to be executed on another thread.
+		 * @param arguments The arguments needed by the work function.
+		 * @return A promise object of the function return.
+		 */
+		template<class Function, class... Arguments, class ReturnType = std::invoke_result_t<Function, Arguments...>>
+		std::future<ReturnType> IssueWork(Function&& function, Arguments&&... arguments)
+		{
+			return mWorkers[GetNextIndex(mIndex++)].IssueWork(function, arguments);
+		}
+
+		/**
+		 * Get the work group size.
+		 * 
+		 * @return The work group size.
+		 */
+		uint64 GetWorkGroupSize() const { return mWorkers.size(); }
+
+		/**
+		 * Resize the work group.
+		 * 
+		 * @param size The required size.
+		 */
+		void ResizeWorkGroup(uint64 size) { mWorkers.resize(size); }
+
+	private:
+		/**
+		 * Get the index of the next position to assign the work.
+		 * 
+		 * @param index The previous index.
+		 * @return The index.
+		 */
+		uint64 GetNextIndex(uint64 index) const { return index % mWorkers.size(); }
+
+	private:
+		std::vector<Worker> mWorkers = {};
+		uint64 mIndex = 0;
+	};
+}
